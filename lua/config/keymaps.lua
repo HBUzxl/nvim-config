db9291c2b45db75796336c37236253624bdf2b31
ZXL <zxl2024@qq.com>
2025-04-20 06:08:52 -0400

    feat: 缓冲区标签页切换快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,13 @@ local keymap = vim.keymap.set
 keymap("n", "<leader>w", ":w<CR>")     -- 保存
 keymap("n", "<leader>q", ":q<CR>")     -- 退出
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>") -- 打开nvim-tree
+
+
+
+-- 标签页切换
+keymap("n", "gt", ":tabnext<CR>", { silent = true })
+keymap("n", "gT", ":tabprevious<CR>", { silent = true })
+
+-- 设置切换缓冲区的快捷键
+keymap("n", "<Tab>", ":bnext<CR>", { silent = true })
+keymap("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,7 @@ vim.opt.expandtab = true       -- 用空格代替tab
 vim.opt.shiftwidth = 2         -- 缩进宽度
 vim.opt.tabstop = 2            -- tab宽度
 vim.opt.termguicolors = true   -- 终端真彩色支持
-vim.opt.clipboard = "unnamedplus"  -- 系统剪切板
+-- vim.opt.clipboard = "unnamedplus"  -- 系统剪切板
 
+vim.opt.timeout = true
+vim.opt.timeoutlen=500
