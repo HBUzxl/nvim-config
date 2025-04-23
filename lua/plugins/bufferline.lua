ebb807746ffd4fc836dfbaf83b8ddbc161fdc414
HBUzxl <zxl2024@qq.com>
2025-04-23 15:28:02 +0800

    fix: 标签页位置

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,18 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "kyazdani42/nvim-web-devicons" },
   config = function()
-    require("bufferline").setup{}
+    require("bufferline").setup{
+      options = {
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        }
+      }
+    }
   end
 }
 
